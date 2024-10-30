07aedafa52c3ebba826c4ef33205537ec8b7307a
zjiang10 <zjiang10@slb.com>
2024-10-30 17:07:46 +0800

    refactor: remove regular expression to avoid confusion

--- WordFrequency/WordFrequencyGame.cs
+++ WordFrequency/WordFrequencyGame.cs
@@ -8,47 +8,46 @@ namespace WordFrequencyStorage
     {
         public string GetResult(string inputStr)
         {
-            if (Regex.Split(inputStr, @"\s+").Length == 1)
+            if (inputStr.Split(new char[] { ' ', '\t', '\n', '\r' }).Where(word => !string.IsNullOrWhiteSpace(word)).Count() == 1)
             {
                 return inputStr + " 1";
             }
-            else
+        
+            //split the input string with 1 to n pieces of spaces
+            string[] arr = Regex.Split(inputStr, @"\s+");
+
+            List<WordFrequency> inputList = new List<WordFrequency>();
+            foreach (var s in arr)
             {
-                //split the input string with 1 to n pieces of spaces
-                string[] arr = Regex.Split(inputStr, @"\s+");
-
-                List<WordFrequency> inputList = new List<WordFrequency>();
-                foreach (var s in arr)
-                {
-                    WordFrequency input = new WordFrequency(s, 1);
-                    inputList.Add(input);
-                }
-
-                //get the map for the next step of sizing the same word
-                Dictionary<string, List<WordFrequency>> map = GetListMap(inputList);
-
-                List<WordFrequency> list = new List<WordFrequency>();
-                foreach (var entry in map)
-                {
-                    WordFrequency input = new WordFrequency(entry.Key, entry.Value.Count);
-                    list.Add(input);
-                }
-
-                inputList = list;
-
-                inputList.Sort((w1, w2) => w2.getFrequency - w1.getFrequency);
-
-                List<string> strList = new List<string>();
-
-                //stringJoiner joiner = new stringJoiner("\n");
-                foreach (WordFrequency w in inputList)
-                {
-                    string s = w.getWordName + " " + w.getFrequency;
-                    strList.Add(s);
-                }
-
-                return string.Join("\n", strList.ToArray());
+                WordFrequency input = new WordFrequency(s, 1);
+                inputList.Add(input);
             }
+
+            //get the map for the next step of sizing the same word
+            Dictionary<string, List<WordFrequency>> map = GetListMap(inputList);
+
+            List<WordFrequency> list = new List<WordFrequency>();
+            foreach (var entry in map)
+            {
+                WordFrequency input = new WordFrequency(entry.Key, entry.Value.Count);
+                list.Add(input);
+            }
+
+            inputList = list;
+
+            inputList.Sort((w1, w2) => w2.getFrequency - w1.getFrequency);
+
+            List<string> strList = new List<string>();
+
+            //stringJoiner joiner = new stringJoiner("\n");
+            foreach (WordFrequency w in inputList)
+            {
+                string s = w.getWordName + " " + w.getFrequency;
+                strList.Add(s);
+            }
+
+            return string.Join("\n", strList.ToArray());
+            
         }
 
         private Dictionary<string, List<WordFrequency>> GetListMap(List<WordFrequency> inputList)
