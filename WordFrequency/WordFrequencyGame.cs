4578f429cd616bed4cf0bc76ddf7cf47f1eab9fb
yyu26 <yyu26@slb.com>
2024-10-30 19:10:40 +0800

    refactor: extract method List

--- WordFrequency/WordFrequencyGame.cs
+++ WordFrequency/WordFrequencyGame.cs
@@ -19,19 +19,31 @@ namespace WordFrequency
 
             Dictionary<string, List<WordInput>> listWordInputMap = GetListMap(inputList);
 
-            List<WordInput> list = listWordInputMap
-                .Select(entry => new WordInput(entry.Key, entry.Value.Count))
-                .ToList();
+            var list = List(listWordInputMap);
 
             inputList = list;
 
-            list.Sort((w1, w2) => w2.WordCount - w1.WordCount);
+            inputList.Sort((w1, w2) => w2.WordCount - w1.WordCount);
 
+            var strList = StrList(inputList);
+
+            return string.Join("\n", strList.ToArray());
+        }
+
+        private static List<string> StrList(List<WordInput> inputList)
+        {
             List<string> strList = inputList
                 .Select(w => $"{w.WordValue} {w.WordCount}")
                 .ToList();
+            return strList;
+        }
 
-            return string.Join("\n", strList.ToArray());
+        private static List<WordInput> List(Dictionary<string, List<WordInput>> listWordInputMap)
+        {
+            List<WordInput> list = listWordInputMap
+                .Select(entry => new WordInput(entry.Key, entry.Value.Count))
+                .ToList();
+            return list;
         }
 
         private static List<WordInput> WordInputs(string[] splitedStrings)
